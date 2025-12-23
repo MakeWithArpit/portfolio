b176d2e64bf6358d0298185b981af357e06ee4be
Arpit <arpit.gangwar061@gmail.com>
2025-12-23 09:23:12 +0530

    makr dynimic - short_description

--- portfolio/homee/views.py
+++ portfolio/homee/views.py
@@ -5,4 +5,5 @@ data = {}
 
 def home(request):
     data['title'] = "Arpit Gangwar - Portfolio"
+    data['short_description'] = """Tech enthusiast building practical solutions with IoT, Python, and Django. Transforming ideas into real-world applications."""
     return render(request, "portfolio_html.html", data)
