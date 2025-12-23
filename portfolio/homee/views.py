af6eb2b43f419d1fb1e98a067f4e4f680e07e468
Arpit <arpit.gangwar061@gmail.com>
2025-12-23 09:59:33 +0530

    make dynamic - profile photo

--- portfolio/homee/views.py
+++ portfolio/homee/views.py
@@ -5,10 +5,14 @@ data = {}
 
 def home(request):
     data['title'] = "Arpit Gangwar - Portfolio"
+    
     data['short_description'] = """Tech enthusiast building practical solutions with IoT, Python, and Django. Transforming ideas into real-world applications."""
-    data['github_url'] = "https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg"
+    
+    data['github_url'] = "https://github.com/MakeWithArpit"
     data['linkedin_url'] = "https://www.linkedin.com/in/arpit-gangwar"
     data['youtube_url'] = "https://www.youtube.com/@arpitgangwar-0.1"
     data['email'] = "arpit.gangwar061@gmail.com"
-    print(data['github_url'])
+
+    data['profile_image'] = "https://i.ibb.co/ymffYc9Q/Photo.jpg"
+
     return render(request, "portfolio_html.html", data)
