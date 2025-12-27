c2b6506799cc347ccf66b5f71bffc128580055a5
Arpit <arpit.gangwar061@gmail.com>
2025-12-27 12:21:24 +0530

    updating urls.py according to media

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from homee import views
 
@@ -6,3 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
