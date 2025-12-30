02e57605b2bf44327a6738d3e369a51f101ccf96
Arpit <arpit.gangwar061@gmail.com>
2025-12-30 23:14:40 +0530

    Disable debug mode and update allowed hosts, and set static root

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gwsyvlh=#8*m8f41y&u5(#5uxu91wh@$s=+uz(y5ehj)z+-iut'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Application definition
